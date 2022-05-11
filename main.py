1e18b279a027db4ef6d81d55f60666f695dacd2c
sandeep <sandeep.c.mohanty@apisero.com>
2022-05-11 09:13:27 +0530

    Day 1

---
+++ main.py
@@ -0,0 +1,21 @@
+#  wap that prints the some notes. It should print:
+#Day 1 - Python Print Function
+#Tge function is declared like this:
+#print('what to print')
+
+
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+# band name generator
+
+city = input("Name the city you grew up in?")
+pet = input("Name the name of a pet ")
+
+your_bandname = city+" "+pet
+
+print(your_bandname)
+
